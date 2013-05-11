7adb8203fcdae1d30ac657028a98e4a58a31523f
James Halliday <mail@substack.net>
2013-05-11 02:59:27 -0700

    "r." align mode

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-module.exports = function (rows, opts) {
+module.exports = function (rows_, opts) {
     if (!opts) opts = {};
     var hsep = opts.hsep === undefined ? '  ' : opts.hsep;
     var align = opts.align || [];
+    
+    var dotsizes = rows_.reduce(function (acc, row) {
+        row.forEach(function (c, ix) {
+            var n = String(c.split('.')[1] || '').length;
+            if (!acc[ix] || n > acc[ix]) acc[ix] = n;
+        });
+        return acc;
+    }, []);
+    
+    var rows = rows_.map(function (row) {
+        return row.map(function (c, ix) {
+            if (align[ix] === 'r.') {
+                var size = dotsizes[ix] + (/\./.test(c) ? 1 : 2)
+                    - (String(c).split('.')[1] || '').length
+                ;
+                return c + Array(size).join(' ');
+            }
+            else return c;
+        });
+    });
     
     var sizes = rows.reduce(function (acc, row) {
         row.forEach(function (c, ix) {
@@ -10,13 +30,12 @@ module.exports = function (rows, opts) {
         });
         return acc;
     }, []);
+    
     return rows.map(function (row) {
         return row.map(function (c, ix) {
             var s = Array(sizes[ix] - c.length + 1).join(' ');
-            if (align[ix] === 'r') {
+            if (align[ix] === 'r' || align[ix] === 'r.') {
                 return s + c;
-            }
-            if (align[ix] === 'r.') {
             }
             if (align[ix] === 'c') {
             }
